7fcaac1d3f1420e001ff3bf6be1376cc439afb49
Shane Wiseman <shane.db.wiseman@gmail.com>
2018-12-16 23:44:51 -0500

    removed old config variables from Config class

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,4 @@
 class Config:
-    DNS_USER_ID = "dns-updater@total-display-138423.iam.gserviceaccount.com" # The GCP Service Account ID
-    DNS_KEY = "config/google.json" 		# The path to the JSON-key file (relative to the project's root)
-    DNS_PROJECT_NAME = "total-display-138423" 	# Your Google Cloud Platform project name
-
-# Example settings for creating an test.mydomain.com A record that points to your current IP
     A_RECORD_TTL_SECONDS = 600 	# The desired TTL for your DNS record
     RECORD_NAME = "shanedbwiseman.com." 		# The record name
     ZONE_FQDN = "shanedbwiseman.com." # The domain zone name (usually the domain with an ending `.`)
