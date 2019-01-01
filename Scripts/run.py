7293812d0d54975e6f2fa5413fc8d7fc9872485b
Shane Wiseman <shane.db.wiseman@gmail.com>
2018-12-31 20:56:15 -0500

    changed run.py to execute in loop

--- Scripts/run.py
+++ Scripts/run.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+import time
 
 from GCPDNSUpdater import Updater, RequestRecord, GCPDNSUpdaterException, Config
 
@@ -7,16 +8,26 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 def main():
-    log.warn("=====================================================")
-    ip = check_ip()
 
-    log.info("Found IP: {}".format(ip))
+    old_ip = None
 
-    rr = generate_request_record(ip)
+    while True:
+        found_ip = check_ip()
 
-    update_record(rr)
+        log.info("Found IP: {}".format(found_ip))
 
-    log.warn("=====================================================")
+        if found_ip != old_ip:
+            log.warn("=====================================================")
+
+            rr = generate_request_record(found_ip)
+
+            update_record(rr)
+
+            old_ip = found_ip
+
+            log.warn("=====================================================")
+
+        time.sleep(300)
 
 def check_ip():
     return requests.get("https://api.ipify.org").text
@@ -25,7 +36,7 @@ def generate_request_record(ip):
     return RequestRecord(ip, Config.HOSTNAME, Config.TTL)
 
 def update_record(rr):
-    updater = Updater(Config.ZONE, "../GCPDNSUpdater/config/google.json")
+    updater = Updater(Config.ZONE, "GCPDNSUpdater/config/google.json")
 
     try:
         updater.update_record(rr)
